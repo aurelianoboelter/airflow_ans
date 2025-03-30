5dd2890b37515e5e7b1bdcd56f84b349f3259c09
Aurelio <aurelio.boelter@gmail.com>
2025-03-30 18:17:21 -0300

    segund commit dags

--- dags/exemplo_01.py
+++ dags/exemplo_01.py
@@ -1,22 +1,38 @@
 from time import sleep
 
+from airflow.decorators import dag, task
+
+@dag(
+         dag_id="dag_inicial",
+         schedule_interval='@daily', 
+         start_date='2023-10-01', 
+         catchup=False, 
+         tags=['Iniciando'])
 def pipeline():
+     
+
+    @task
 
     def primeira_atividade():
         print("minha primeira atividade! - Executado Ok ")
         sleep(2)
+    @task
 
     def segunda_atividade():
         print("minha segunda atividade! - Executado Ok")
         sleep(2)
+    @task
 
     def terceira_atividade():
         print("minha terceira atividade - Executado Ok")
         sleep(2)
 
     def pipeline():
-        primeira_atividade()
-        segunda_atividade()
-        terceira_atividade()
+        t1 =primeira_atividade()
+        t2 =segunda_atividade()
+        t3 =terceira_atividade()
+
+        t1 >> t2 >> t3
+    pipeline()
     
 
